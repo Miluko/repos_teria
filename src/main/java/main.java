220551cf3bd00aefb7b68deb9ac16dbab828ed2a
Usuario <Usuario@LAPTOP-453FQ6JN>
2021-03-14 19:56:26 +0100

    commit de prueba1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,7 @@
 
 import cuentas.CCuenta;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Usuario
- */
 public class main {
     public static void main(String[] args) {
         CCuenta cuenta1;
